1bbf326ecbc2a89c1937f1b44325405801fc905c
Saketh Kunuku <SakethKunuku205@gmail.com>
2026-03-14 23:29:41 +0530

    added codes --> variables

---
+++ variables/Basic01.java
@@ -0,0 +1,8 @@
+package variables;
+
+public class Basic01{
+    public static void main(String[] args){
+        int a = 10;
+        System.out.println(a);
+    }
+}
---
+++ variables/Basic02.java
@@ -0,0 +1,9 @@
+package variables;
+public class Basic02 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        System.out.println(num1 + num2);
+    }
+}
+
