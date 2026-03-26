e0d1c91b89e6b0fafc8c59465cd50bc8c41bd36e
Saketh Kunuku <SakethKunuku205@gmail.com>
2026-03-26 23:39:02 +0530

    code updated with explanation

--- method_overriding/calculator.java
+++ method_overriding/calculator.java
@@ -16,3 +16,9 @@ public class calculator {
         System.out.println(obj.add(2,3));
     }
 }
+
+/*
+method overriding is like, assigining the highest priority to itself first, if not possible, use method in super class.
+here in the example, when the AdvCalc has no add method present in it, it can calls the method add() in the super class Calc to get output of 5, 
+but when the AdvCalc has the method add(), as a priority overriding, it calls the add() method in it's own class itself...!
+ */
